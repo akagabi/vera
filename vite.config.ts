fb8e695a9f6e6bb778bb939f49521f6abf7c2128
Gabriel Bueno <gabriel@detail.co>
2025-02-28 00:44:52 +0100

    Update repository name to vera for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
   // Base path for GitHub Pages deployment
-  base: "/currency-converter/",
+  base: "/vera/",
   plugins: [
     react(),
     VitePWA({
