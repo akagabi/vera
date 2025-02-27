b5708c04b807b101ad1ccd7adb68060a2a0bed6b
Gabriel Bueno <gabriel@detail.co>
2025-02-28 00:40:56 +0100

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Base path for GitHub Pages deployment
+  base: "/currency-converter/",
   plugins: [
     react(),
     VitePWA({
